67b8a4e148fbd7b4c9963b1e9a5026e1132a23ae
ADJStreams <adjstreams@gmail.com>
2023-03-18 17:20:39 +0000

    new and improved

--- create-music-clips.ps1
+++ create-music-clips.ps1
@@ -1,5 +1,6 @@
 param (
-    [string]$subtitle = "MOST PLAYED THIS WEEK",
+    [string]$title = "MOST PLAYED THIS WEEK",
+    [string]$subtitle = "ON SPOTIFY",
     [string]$input_dir = (Get-Location).Path,
     [string]$output_dir = "output",
 	[string]$bg_video_dir = "background_videos",
@@ -11,11 +12,11 @@ param (
 function Show-Usage {
     $scriptName = Split-Path -Path $MyInvocation.ScriptName -Leaf
     Write-Host "Invalid usage:"
-    Write-Host "  .\$scriptName [-subtitle <subtitle_text>] [-input_dir <input_directory>] [-output_dir <output_directory>] [-bg_video_dir <background_video_directory>] [-font <font_file>] [-logo_image <logo_image_file>] [-genre <genre_prefix>]"
+    Write-Host "  .\$scriptName [-title <title_text>] [-subtitle <subtitle_text>] [-input_dir <input_directory>] [-output_dir <output_directory>] [-bg_video_dir <background_video_directory>] [-font <font_file>] [-logo_image <logo_image_file>] [-genre <genre_prefix>]"
     exit
 }
 
-$validParameters = @("subtitle", "input_dir", "output_dir", "bg_video_dir", "font", "logo_image", "genre")
+$validParameters = @("title", "subtitle", "input_dir", "output_dir", "bg_video_dir", "font", "logo_image", "genre")
 
 for ($i = 0; $i -lt $args.Count; $i += 2) {
     $parameterName = $args[$i].TrimStart('-')
@@ -38,11 +39,14 @@ $count = 0
 $files = @()
 
 
-if ($genre) {
-    $bg_video_filter = "$genre_*.mp4"
+if ($genre -ne "") {
+    write-Host "genre is $genre"
+    $bg_video_filter = "$($genre)_*.mp4"
 } else {
     $bg_video_filter = "*.mp4"
 }
+
+Write-Host "Using filter $bg_video_filter"
 
 Get-ChildItem -Path $bg_video_dir -Filter $bg_video_filter | ForEach-Object {
     $files += $_.FullName
@@ -54,12 +58,13 @@ $count--
 Get-ChildItem -Path $input_dir -Filter "*.mp3" | ForEach-Object {
     $randomIndex = (Get-Random -Maximum ($count + 1))
     $bg_video = $files[$randomIndex]
+    Write-Host "Using $bg_video"
 
 	$ffmpeg_inputs = @("-stream_loop", "-1", "-t", $clip_duration, "-i", "`"$bg_video`"", "-ss", $clip_start, "-t", $clip_duration, "-i", "`"$($_.FullName)`"", "-i", "`"$logo_image`"")
 
     Write-Host "GENERATING 1920 X 1080 clip"
-    &ffmpeg $ffmpeg_inputs -filter_complex "`"[0:v]scale=1920x1080,setpts=PTS-STARTPTS[bkg];$afade_filter;[2:v]scale=400:-1,setsar=1[logo];[bkg][logo]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2[bg1];[1:a]showwaves=s=1920x200:mode=p2p:colors=white[audioviz];[bg1][audioviz]overlay=0:main_h-overlay_h[v];[v]drawtext=fontfile=$($font):text='$($_.BaseName)':fontcolor=white:fontsize=72:x=(w-text_w)/2:y=h-80,drawtext=fontfile=$($font):text='$subtitle':fontcolor=white:fontsize=120:x=(w-text_w)/2:y=30[vout]`"" $ffmpeg_outputs "$output_dir\$($_.BaseName)_1920x1080.mp4"
+    &ffmpeg $ffmpeg_inputs -filter_complex "`"[0:v]scale=1920x1080,setpts=PTS-STARTPTS[bkg];$afade_filter;[2:v]scale=400:-1,setsar=1[logo];[bkg][logo]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2[bg1];[1:a]showwaves=s=1920x200:mode=cline:colors=white[audioviz];[bg1][audioviz]overlay=0:main_h-overlay_h[v];[v]drawtext=fontfile=$($font):text='$($_.BaseName)':fontcolor=white:fontsize=72:x=(w-text_w)/2:y=h-250,drawtext=fontfile=$($font):text='$title':fontcolor=white:fontsize=120:x=(w-text_w)/2:y=30,drawtext=fontfile=$($font):text='$subtitle':fontcolor=white:fontsize=100:x=(w-text_w)/2:y=30+text_h+50[vout]`"" $ffmpeg_outputs "$output_dir\$($_.BaseName)_1920x1080.mp4"
 	
 	Write-Host "GENERATING 1080 X 1920 clip"
-	ffmpeg $ffmpeg_inputs -filter_complex "`"[0:v]scale=-1:1920,crop=1080:1920:(iw-1080)/2:0,setpts=PTS-STARTPTS[bkg];$afade_filter;[2:v]scale=400:-1,setsar=1[logo];[bkg][logo]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2[bg1];[1:a]showwaves=s=1080x400:mode=p2p:colors=white[audioviz];[bg1][audioviz]overlay=0:main_h-overlay_h[v];[v]drawtext=fontfile=$($font):text='$subtitle':fontcolor=white:fontsize=120:x=(w-text_w)/2:y=55,drawtext=fontfile=$($font):text='$($_.BaseName)':fontcolor=white:fontsize=72:x=(w-text_w)/2:y=15+text_h+130[vout]`"" $ffmpeg_outputs "$output_dir\$($_.BaseName)_1080x1920.mp4"
+	ffmpeg $ffmpeg_inputs -filter_complex "`"[0:v]scale=-1:1920,crop=1080:1920:(iw-1080)/2:0,setpts=PTS-STARTPTS[bkg];$afade_filter;[2:v]scale=600:-1,setsar=1[logo];[bkg][logo]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2[bg1];[1:a]showwaves=s=1080x400:mode=cline:colors=white[audioviz];[bg1][audioviz]overlay=0:main_h-overlay_h[v];[v]drawtext=fontfile=$($font):text='$title':fontcolor=white:fontsize=120:x=(w-text_w)/2:y=55,drawtext=fontfile=$($font):text='$subtitle':fontcolor=white:fontsize=100:x=(w-text_w)/2:y=30+text_h+100,drawtext=fontfile=$($font):text='$($_.BaseName)':fontcolor=white:fontsize=72:x=(w-text_w)/2:y=h-500[vout]`"" $ffmpeg_outputs "$output_dir\$($_.BaseName)_1080x1920.mp4"
 }
